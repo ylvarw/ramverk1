5b6f356ab7ff30d8adf9c1b993362af470e506cb
ylvarw <ylvarw@gmail.com>
2020-11-10 11:46:28 +0100

    new controller in route

---
+++ config/router/300_ylvanController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Load the stylechooser as a controller class.
+ */
+return [
+    "routes" => [
+        [
+            "info" => "Ylvan Controller.",
+            "mount" => "Ylvan",
+            "handler" => "\Ylvan\Controller\YlvanController",
+        ],
+    ]
+];
